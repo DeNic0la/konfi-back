71c4ee744b8eacf57aa8db51d727b171b9f72746
DeNic0la <57986114+DeNic0la@users.noreply.github.com>
2025-09-10 04:27:28 +0200

    add queue for errors

--- src/main/java/ch/denic0la/konfi/table/WebSocketConfig.java
+++ src/main/java/ch/denic0la/konfi/table/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.setApplicationDestinationPrefixes("/live");
-    //registry.setUserDestinationPrefix("/queue").enableSimpleBroker("/queue");
+    registry.setUserDestinationPrefix("/queue").enableSimpleBroker("/queue");
     registry.enableSimpleBroker("/table");//.setHeartbeatValue(new long[] {10000, 20000}).setTaskScheduler(heartBeatScheduler());
   }
 
